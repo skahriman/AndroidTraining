83180261ec6b555e42a0bede9e4780a15b559353
skahriman <sefak.kahriman@gmail.com>
2018-03-31 13:34:52 -0400

    a little bit modifications on Jungle problem.

--- Jungle/src/Monkey.java
+++ Jungle/src/Monkey.java
@@ -1,36 +1,37 @@
 
 public class Monkey extends Animal{
-
-	String sound;
-	
-	public Monkey(String kind, int eneryLevel) {
-		super(kind, eneryLevel);
-		this.sound = "Monkeys are screaming";
-	}
-	
-	public void eatFood() {
-		this.eneryLevel = eneryLevel + 2;
-	}
-
-	public void makeSound() {
-		if(this.eneryLevel - 4 < 0)
-			System.out.println("can not perform action: No enaugh energy...");
-		else {
-			this.eneryLevel = eneryLevel - 4;
-			System.out.println(this.getName() + " is speaking...");
-			System.out.println(this.getName() + " energy: " + this.eneryLevel);
-		}
-	}
-	
-	public void play() {
-		if(this.eneryLevel >= 8) {
-			this.eneryLevel = this.eneryLevel - 8;
-			System.out.println(this.getName() + " is playing");
-			System.out.println("Oooo Oooo Oooo");
-			System.out.println(this.getName() + " energy: " + this.eneryLevel);
-		}
-		else
-			System.out.println(this.getName() +  " doesn't have enaugh energy");
-	}
-
+    
+    String sound;
+    
+    public Monkey(String kind, int eneryLevel) {
+        super(kind, eneryLevel);
+        this.sound = "Monkeys are screaming";
+    }
+    
+    public void eatFood(String string) {
+        this.eneryLevel = eneryLevel + 2;
+    }
+    
+    public void makeSound() {
+        if(this.eneryLevel - 4 < 0)
+            System.out.println("can not perform action: No enaugh energy...");
+        else {
+            this.eneryLevel = eneryLevel - 4;
+            System.out.println(this.getName() + " is speaking...");
+            System.out.println(this.getName() + " energy: " + this.eneryLevel);
+        }
+    }
+    
+    public void play() {
+        if(this.eneryLevel >= 8) {
+            this.eneryLevel = this.eneryLevel - 8;
+            System.out.println(this.getName() + " is playing");
+            System.out.println("Oooo Oooo Oooo");
+            System.out.println(this.getName() + " energy: " + this.eneryLevel);
+        }
+        else
+            System.out.println(this.getName() +  " doesn't have enaugh energy");
+    }
+    
 }
+
--- Jungle/src/Tiger.java
+++ Jungle/src/Tiger.java
@@ -1,13 +1,23 @@
 
 public class Tiger extends Animal {
-
-	String sound;
-	
-	public Tiger(String name, int eneryLevel) {
-		super(name, eneryLevel);
-	}
-
-	public void sleep() {
-		this.eneryLevel = eneryLevel + 5;
-	}
+    
+    String sound;
+    
+    public Tiger(String name, int eneryLevel) {
+        super(name, eneryLevel);
+    }
+    
+    public void sleep() {
+        this.eneryLevel = eneryLevel + 5;
+    }
+    
+    public void eatFood(String food) {
+        if(food.equals("grain")) {
+            System.out.println("Tiger can not eat grain!!!");
+            System.out.println(this.getName() + " energy: " + this.eneryLevel);
+        }
+        else
+            super.eatFood(food);
+    }
 }
+
