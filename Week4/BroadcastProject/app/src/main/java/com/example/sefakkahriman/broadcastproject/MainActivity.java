3c6c16e4f65567fbb9b6841baf1ff0457862c9ec
skahriman <sefak.kahriman@gmail.com>
2018-04-09 20:03:52 -0400

    Number 1 is done.

--- Week4/BroadcastProject/app/src/main/java/com/example/sefakkahriman/broadcastproject/MainActivity.java
+++ Week4/BroadcastProject/app/src/main/java/com/example/sefakkahriman/broadcastproject/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.sefakkahriman.broadcastproject;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.sefakkahriman.broadcastproject.recievers.MyReceiver;
 
 public class MainActivity extends AppCompatActivity {
+    private MyReceiver myReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //register myCustomReceiver
+        myReceiver = new MyReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
+        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
+
+        registerReceiver(myReceiver, intentFilter);
+
+    }
+
+    public void sendBroadcast(View view) {
+        Intent intent = new Intent();
+        intent.setAction("sendBroadcastAction");
+        sendBroadcast(intent);
+    }
+
+    public void onClickSendOrderedBroadcast(View view) {
+        Intent intent = new Intent();
+        intent.setAction("sendOrderedBroadcastAction");
+        sendOrderedBroadcast(intent, "hello there");
+    }
+
 }
---
+++ Week4/BroadcastProject/app/src/main/java/com/example/sefakkahriman/broadcastproject/recievers/MyReceiver.java
@@ -0,0 +1,45 @@
+package com.example.sefakkahriman.broadcastproject.recievers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Log.d("MY_RECEIVER_TAG ", "onReceive: ");
+
+        switch (intent.getAction()) {
+            case Intent.ACTION_AIRPLANE_MODE_CHANGED:
+
+                boolean status1 = intent.getBooleanExtra("state", false);
+
+                Toast.makeText(context, "Airplane mode " + status1, Toast.LENGTH_SHORT).show();
+                break;
+
+            case Intent.ACTION_POWER_CONNECTED:
+                Toast.makeText(context, "Charger connected", Toast.LENGTH_SHORT).show();
+
+                break;
+
+            case Intent.ACTION_POWER_DISCONNECTED:
+
+                Toast.makeText(context, "Charger disconnected", Toast.LENGTH_SHORT).show();
+
+                break;
+
+            case Intent.ACTION_SCREEN_ON:
+                Toast.makeText(context, "Screen is off ", Toast.LENGTH_SHORT).show();
+                break;
+
+            case Intent.ACTION_SCREEN_OFF:
+                Toast.makeText(context, "Screen is on", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+}
