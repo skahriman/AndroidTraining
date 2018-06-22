e9d49b3b6dc1f52cdc83a0bffe53a42aafa445e0
skahriman <sefak.kahriman@gmail.com>
2018-06-22 13:20:58 -0400

    click handler is added to recyclerview items

---
+++ ChaseMobileAppDemo/app/src/main/java/com/example/sefakkahriman/chasemobileappdemo/AccountDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.sefakkahriman.chasemobileappdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_detail);
+    }
+}
--- ChaseMobileAppDemo/app/src/main/java/com/example/sefakkahriman/chasemobileappdemo/adapter/MyAdapter.java
+++ ChaseMobileAppDemo/app/src/main/java/com/example/sefakkahriman/chasemobileappdemo/adapter/MyAdapter.java
@@ -1,13 +1,17 @@
 package com.example.sefakkahriman.chasemobileappdemo.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.sefakkahriman.chasemobileappdemo.AccountDetailActivity;
 import com.example.sefakkahriman.chasemobileappdemo.R;
 import com.example.sefakkahriman.chasemobileappdemo.adapter_model.ListItem;
 
@@ -34,10 +38,18 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
 
-        ListItem item = listitems.get(position);
+        final ListItem item = listitems.get(position);
         holder.title.setText(item.getTitle());
         holder.balance.setText(item.getBalance());
         holder.availableBalance.setText(item.getAvailableBalance());
+
+        //This part is handling click on RecyclerView items
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.startActivity(new Intent(context, AccountDetailActivity.class));
+            }
+        });
 
     }
 
@@ -53,11 +65,17 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         public TextView balance;
         public TextView availableBalance;
 
+        //This part is handling click on RecyclerView items
+        LinearLayout linearLayout;
+
         public ViewHolder(View itemView) {
             super(itemView);
             this.title = itemView.findViewById(R.id.title);
             this.balance = itemView.findViewById(R.id.balance);
             this.availableBalance = itemView.findViewById(R.id.availableBalance);
+
+            //This part is handling click on RecyclerView items
+            linearLayout = itemView.findViewById(R.id.linearLayout);
         }
     }
 }
