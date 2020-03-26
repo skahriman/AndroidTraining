5d506049849d110b751cf8505132c276309bc0c9
sefak kahriman <SefakKahriman@root.local>
2020-03-25 20:17:08 -0400

    ListView Demo fixed

--- MyExtras/ListViewDemo/app/src/main/java/com/sk/listviewdemo/MainActivity.java
+++ MyExtras/ListViewDemo/app/src/main/java/com/sk/listviewdemo/MainActivity.java
@@ -3,9 +3,12 @@ package com.sk.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +20,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] teams = getResources().getStringArray(R.array.teams);
+        String[] teams = {"Turkey", "USA", "Germany", "India"};
         mListView = findViewById(R.id.listView);
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, teams);
 
         mListView.setAdapter(adapter);
+        
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, adapterView.getItemAtPosition(i) + " is selected", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
