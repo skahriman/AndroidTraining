894e92269c5118818529a5e92d0d995ef8960708
sefak kahriman <SefakKahriman@root.local>
2020-04-07 18:51:06 -0400

    load images from internet using asynctask with Picasso

--- MyExtras/LoadingImageWityAsyncTask/app/src/main/java/com/exmaple/android/loadingimagewityasynctask/MainActivity.java
+++ MyExtras/LoadingImageWityAsyncTask/app/src/main/java/com/exmaple/android/loadingimagewityasynctask/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
+import com.squareup.picasso.Picasso;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -21,18 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
     private static String IMAGE_PATH = "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg";
     ImageView mImageView;
-    Button downlaodBtn;
-    ProgressDialog mProgressDialog;
-
+    Button downloadBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mImageView = findViewById(R.id.iv_image);
-        downlaodBtn = findViewById(R.id.btn_button);
+        downloadBtn = findViewById(R.id.btn_button);
 
-        downlaodBtn.setOnClickListener(new View.OnClickListener() {
+        downloadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 new Downloader().execute(IMAGE_PATH);
@@ -40,40 +40,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private class Downloader extends AsyncTask<String, Void, Bitmap> {
+    private class Downloader extends AsyncTask<String, Void, String> {
 
         @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mProgressDialog = new ProgressDialog(MainActivity.this);
-            mProgressDialog.setTitle("Image Downloader");
-            mProgressDialog.setMessage("Downloading...");
-            mProgressDialog.setIndeterminate(false);
-            mProgressDialog.show();
-        }
-
-        @Override
-        protected Bitmap doInBackground(String... strings) {
+        protected String doInBackground(String... strings) {
             String myString = strings[0];
-            Bitmap bm = null;
-
-            try {
-                InputStream is = new URL(myString).openStream();
-                bm = BitmapFactory.decodeStream(is);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return bm;
+            return myString;
         }
 
         @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-            mImageView.setImageBitmap(bitmap);
-            mProgressDialog.dismiss();
+        protected void onPostExecute(String string) {
+            super.onPostExecute(string);
+
+            Picasso
+                    .get()
+                    .load(IMAGE_PATH)
+                    .into(mImageView);
         }
     }
 }
